b9b42fb7833690ed02999bdf949819790dfc27e9
xuanhao <arieshao@vip.qq.com>
2016-12-16 17:02:09 +0800

    optimize some of detail  and add news detailPage

---
+++ MyApplication/app/src/main/java/vip/xuanhao/integration/presenters/ipresenter/DetailPresenter.java
@@ -0,0 +1,10 @@
+package vip.xuanhao.integration.presenters.ipresenter;
+
+import vip.xuanhao.integration.presenters.BasePresenter;
+
+/**
+ * Created by Xuanhao on 2016/12/16.
+ */
+
+public class DetailPresenter extends BasePresenter {
+}
---
+++ MyApplication/app/src/main/java/vip/xuanhao/integration/presenters/ipresenter/IDetailPresenter.java
@@ -0,0 +1,8 @@
+package vip.xuanhao.integration.presenters.ipresenter;
+
+/**
+ * Created by Xuanhao on 2016/12/16.
+ */
+
+public interface IDetailPresenter {
+}
